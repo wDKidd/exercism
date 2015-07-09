6c36e43340b0d8a2a97b03eaec631fa91f0bb04c
Kayla Kidd <kzj.kidd@gmail.com>
2015-07-09 13:44:35 +0200

    splits strings into array values

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,11 @@
+function wordCounter(theString) {
+
+  function splitString(theString, separator) {
+    var arrayKey = theString.split(separator);
+
+    return arrayKey;
+
+  }
+}
+
+module.exports = wordCounter;
