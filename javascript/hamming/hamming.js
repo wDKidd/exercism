eacbaf2cd9e4e6ca4c6a9016ba4749e443bfd3a3
Kayla Kidd <kzj.kidd@gmail.com>
2015-07-09 16:09:20 +0200

    create arrays from input

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,11 @@
+module.exports =
+  function hamDiff(a, b) {
+
+    //create two arrays from input of  'GGACG', 'GGTCG'
+    var strandA = [],
+      strandB = [];
+
+    strandA.push(a);
+    strandB.push(b);
+
+  }
