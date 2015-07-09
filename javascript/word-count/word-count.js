94eceb8cd31fa4d7b179c45e68e042ea8856b57b
Kayla Kidd <kzj.kidd@gmail.com>
2015-07-09 15:25:49 +0200

    First solution without revision

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,8 +1,22 @@
-function wordCounter(theString) {
+'use strict';
 
-  var arrayKey = theString.split(" ");
-  var words = {};
+var wordCounter = function(input) {
+  var counts = {};
+  var words = input.match(/\S+/g);
 
-}
+  words.forEach(function(word) {
+    function getCount(counts, word) {
+      if (counts.hasOwnProperty(word)) {
+        return counts[word] = counts[word] + 1;
+      } else {
+        return counts[word] = 1;
+      }
+    }
+
+    counts[word] = getCount(counts, word);
+  });
+
+  return counts;
+};
 
 module.exports = wordCounter;
